c34807bcbbf41c9d3885e3042a16f28c2155cbf4
Guillermo Vargas <gjvargas@mit.edu>
2014-11-14 15:39:48 -0500

    added express session and passport

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local');
 
 // Database setup
 var mongo = require('mongodb');
@@ -43,6 +46,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({secret: 'supernova', saveUninitialized: true, resave: true}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', routes);
 app.use('/users', users);
