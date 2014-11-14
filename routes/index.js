41c1fac5d9652b9011456294ab28fd14a09863bf
paerhati <faruh.paerhati@gmail.com>
2014-11-14 15:48:48 -0500

    Set up a test view page for our socket chats

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+/* Test real-time chat page */
+router.get('/chat', function(req, res) {
+	res.render('chat', { title: 'Socket Chat Test Page'})
+});
+
 module.exports = router;
