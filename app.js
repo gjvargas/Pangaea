eefc7cf9c225dc08fc0144bc803da415d2d8283d
Eben Bitonte <ebitonte@mit.edu>
2014-11-18 16:29:18 -0500

    fixing some merge conflicts

--- app.js
+++ app.js
@@ -48,6 +48,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({secret: 'supernova', saveUninitialized: true, resave: true}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-	res.render('index', { title: 'Express' });
+	res.render('index', { title: 'Pangaea' });
 });
 
 router.get('/login', function(req, res) {
