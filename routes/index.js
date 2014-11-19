0741b27585d3368f698da462c9e00d5cddeb34ae
Guillermo Vargas <gjvargas@mit.edu>
2014-11-18 23:00:31 -0500

    added some specs

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,26 @@
-// Author: Eben Bitonte
-
+/**
+ * Lead Author: Eben
+ * This file defines the routes for the home page and login.
+ *
+ * (GET) / - Takes the user to the home page.
+ * (POST) /login - Logs the user in.
+ * (GET) /logout - Logs the user out
+ */
 var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 
 var languages = ["English", "Spanish", "French", "Portuguese", "German", "Mandarin", "Korean", "Japanese", "Arabic"];
 
-/*
-	Get home page with login and create new user
-
-	If reached this page through error previously logging in or
-	creating a new user, that error is displayed
+/**
+* Takes the user to the home page.
+*
+* The request has a GET body.
+*
+* We handle several errors that might occur when visiting our site.
+* We render the login page if the user isn't logged in.
+* We redirect the user to their user page if the user is already logged in
+*
 */
 router.get('/', function(req, res) {
 	var flash = req.flash('error');
@@ -24,26 +34,29 @@ router.get('/', function(req, res) {
 		res.redirect('/users/')
 	}
 });
-
-/*
-	Login route
-
-	Logs in current user through passport-
-		* creates a session as well
-		* handles errors of logging in (i.e. wrong password)
-*/
+/**
+ * Logs a user in.
+ *
+ * The request has a POST body that must include a "username" and "password".
+ *
+ * We use passport which handles the whole process of authentication and logging in.
+ *
+ * It also sets a session_id cookie.
+ */
 router.post('/login',
   passport.authenticate('local', { successRedirect: '/users/',
                                    failureRedirect: '/',
                                    failureFlash: true})
 );
 
-/*
-	Logout route
-
-	Logs out of passport-
-		* handles removal of cookies/session
-		* redirects to login page
+/**
+* Logs a user in.
+*
+* The request has a GET body.
+*
+* We use passport which handles the whole process of authentication and logging in.
+*
+* It also removes the session_id cookie.
 */
 router.get('/logout', function(req, res){
   req.logout();
