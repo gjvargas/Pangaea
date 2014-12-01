b8643e01461a9f7e8c30f441935ac47a82ddca8d
Eben Bitonte <ebitonte@mit.edu>
2014-12-01 17:58:36 -0500

    double right container

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,4 +1,7 @@
+var $newExchange = $("<h1> I want to learn... </h1><form action='create_exchange' method='post'><select name='language' id='languageSelect' class='form-control'><% for(var i = 0; i < languages.length; ++i) { %><option value=<%=languages[i]%>> <%=languages[i]%> </option><% } %></select><button class='btn btn-lg btn-success btn-block' id='createButton' type='submit'> Create Exchange </button></form>");
+
 $('.exchangeLink').click(function(event) {
+
 	console.log('LINK CLICK');
 	var exchangeLink = '/exchanges/' + event.target.id;
 	$.ajax({
@@ -13,17 +16,24 @@ $('.exchangeLink').click(function(event) {
 	});
 });
 
+$('#create-new').click(function() {
+	$('#rightContainer').addClass('hidden');
+	$('#newExchange').removeClass('hidden');
+});
+
 var renderMessages = function(messages, user) {
+	$('#rightContainer').removeClass('hidden');
+	$('#newExchange').addClass('hidden');
 	var $messages_container = $("<div>", {id: 'messages_container'});
 
 	messages.forEach(function(i) {
 		console.log(i);
 		var $message = $("<span>", {text: i.content});
 
-		var userMessage = message.author.username == user.username;
+		var userMessage = i.author.username == user.username;
 		var messageClass = userMessage ? "userMessage" : "otherMessage";
 		$message.addClass(messageClass);
-		var timeString = getTimeString(Date.parse(i.time));
+		var timeString = getTimeString(new Date(i.time));
 		$messages_container.append($message);
 	});
 	$('#rightContainer').empty().append($messages_container);
@@ -33,10 +43,13 @@ var renderMessages = function(messages, user) {
 	$('#rightContainer').append($messageInput, $send);
 }
 
+
+
 var getTimeString = function(date) {
 	var hour = date.getHours();
 	hour = hour > 12 ? hour - 12 : hour;
 	hour = hour == 0 ? hour + 12 : hour;
 	var minute = date.getMinutes();
-	return hour.toString() + ":" + minute.toString();
+	minute = minute < 10 ? "0"+minute : minute;
+	return hour + ":" + minute;
 }
