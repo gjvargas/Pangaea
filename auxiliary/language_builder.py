fd33435e5d808761fda529173544fc129c831c7a
Guillermo Vargas <gjvargas@mit.edu>
2014-12-02 16:25:51 -0500

    comments

--- auxiliary/language_builder.py
+++ auxiliary/language_builder.py
@@ -5,13 +5,14 @@ file_out = open('./auxiliary/language_html_blob.html','w')
 file_out.write('<select>\n')
 
 counter = 1;
+# file has each language on two lines
 for line in file_in:
   if counter > 0:
     file_out.write('\t<option value=\"')
-    file_out.write(line.strip()) #split()[0] is hack to remove whitespace at end, d
+    file_out.write(line.strip()) # these are two letter codes
   else:
     file_out.write('\">')
-    file_out.write(line.strip()) #don't feel like looking up regex in python
+    file_out.write(line.strip()) # these are full language strings
     file_out.write('</option>\n')
   counter = counter * -1
 
--- public/javascripts/translate.js
+++ public/javascripts/translate.js
@@ -1,27 +1,38 @@
+/*
+ * This method queries for the translation when the Translate button is clicked.
+ * When the query completes, it updates the page appropriately.
+ */
+
 $(function(){
-
-
-$('button').click(function(event){
-  var input = $('#translate-input').val();
-  if(input === '') {
-    input = ' ';
-  }
-  var data  = {
-    text: input,
-    language1: $('#from-language').val(),
-    language2: $('#to-language').val()
-  };
-  console.log($('#translate-input').val());
-  $.get(
-    '/translator/translate',
-    data,
-    function(res, err){
-      translate(res);
+  // When the button is clicked, translate input
+  $('button').click(function(event){
+    var input = $('#translate-input').val();
+    // handle the null case
+    if(input === '') {
+      input = ' ';
     }
-  );
-});
-
+    // construct data for query: the text to be translated, and the languages of translation
+    var data  = {
+      text: input,
+      language1: $('#from-language').val(),
+      language2: $('#to-language').val()
+    };
+    // perform the ajax query for translation
+    $.get(
+      '/translator/translate',
+      data,
+      function(res, err){
+        //update the page
+        translate(res);
+      }
+    );
+  });
 })
+
+/*
+ * This method is a simple helper method that
+ * updates the html when the translation completes.
+ */
 
 var translate = function(translation) {
   $('#translated').html(translation);
