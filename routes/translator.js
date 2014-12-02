c4736c538f8aa849a9b9c00401914640f56bb4da
Guillermo Vargas <gjvargas@mit.edu>
2014-12-02 16:35:20 -0500

    cleaned up

--- routes/translator.js
+++ routes/translator.js
@@ -1,38 +1,37 @@
 var express = require('express');
 var router = express.Router();
+// Use bing translate
 var bt = require('../public/javascripts/bing_translate.js').init({
-    client_id: 'Pangaea',
-    client_secret: 'PangaeaPangaeaPangaeaPangaea'
-  });
-
-router.get('/', function(req, res) {
-    var obj = {
-      title: 'Pangaea',
-      language1: 'en',
-      language2: 'ro',
-      translated: ''
-    }
-    res.render('translate', obj);
-
-
+  client_id: 'Pangaea',
+  client_secret: 'PangaeaPangaeaPangaeaPangaea'
 });
 
+/*
+ * Renders the inital page.
+ * Mostly used for testing.
+ */
+router.get('/', function(req, res) {
+  var obj = {
+    title: 'Pangaea',
+    translated: ''
+  }
+  res.render('translate', obj);
+});
+
+/*
+ * Queries for the translation of text passed through URL.
+ * Once translation is received from Bing, it is relayed to client.
+ */
 router.get('/translate', function(req, res) {
   var input = req.query.text;
   var language1 = req.query.language1;
   var language2 = req.query.language2;
 
+  // query bing
   bt.translate(input, language1, language2, function(err, out){
     var translation = out.translated_text;
-    var obj = {
-      title: 'words',
-      translated: out.translated_text,
-      language1: language1,
-      language2: language2,
-      text: input
-    };
-
-    res.send(obj.translated);
+    //send translation to client
+    res.send(translation);
   });
 });
 
