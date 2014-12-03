ab041e4e1009d1132e9268adf4654c2f873dac74
Guillermo Vargas <gjvargas@mit.edu>
2014-12-02 19:21:28 -0500

    commenting message mongoose file

--- models/exchange.js
+++ models/exchange.js
@@ -1,14 +1,17 @@
 /**
  * This file defines the mongoose model for an exchange.
  *
- * Each exchange consists of two users. This is
- * represented as an array of users.
+ * Each exchange consists of:
+ *    2 user ids - these represent the users involved in the exchange
+ *    request - a language that one of the users wants to learn
+ *    proficiency - a language that one of the users is fluent in
  */
 
 var mongoose = require('mongoose');
 
 // The exchangeSchema is how we represent an exchange
 var exchangeSchema = new mongoose.Schema({
+
   // Note that this array will always contain exactly 2 users
   users: [{
     type : mongoose.Schema.ObjectId,
