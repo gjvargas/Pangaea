fd2d388cd03e81d7fb98c865b4639e93de2b4ea1
Jonathan Batscha <jbatscha@mit.edu>
2014-12-03 02:32:58 -0500

    got tests working for properly creating users, still need to test exchanges

--- public/javascripts/tests.js
+++ public/javascripts/tests.js
@@ -7,336 +7,350 @@
 //get index page
 
 
-
-/////////////////////
-//duplicate email//
-///////////////////
-var duplicate_email_req = {
-	username : "user2",
-	email: "user1@mit.edu",
-	password: "password1",
-	proficiencies: ["English"],
-	desires: ["Spanish"]
-}
-
-var duplicate_email_res;
-
-$.ajax({
-	type: "POST",
-	url: "/users/create",
-	data: duplicate_email_req,
-	dataType: "json"
-}).done(function(result){
-	console.log(result)
-	duplicate_email_res = result;
-}).fail(function(err){
-	console.log(err);
-	duplicate_email_res = err;
-});
-
-/////////////////////
-//duplicate username//
-///////////////////
-var duplicate_username_req = {
-	username : "user1",
-	email: "user2@mit.edu",
-	password: "password1",
-	proficiencies: ["English"],
-	desires: ["Spanish"]
-}
-
-var duplicate_username_res;
-
-$.ajax({
-	type: "POST",
-	url: "/users/create",
-	data: duplicate_username_req,
-	dataType: "json"
-}).done(function(result){
-	console.log(result)
-	duplicate_username_res = result;
-}).fail(function(err){
-	console.log(err);
-	duplicate_username_res = err;
-});
-
-
-
-/////////////////////
-//short password//
-///////////////////
-var short_password_req = {
-	username : "user1",
-	email: "user1@mit.edu",
-	password: "123",
-	proficiencies: ["English"],
-	desires: ["Spanish"]
-}
-
-var short_password_res;
-
-$.ajax({
-	type: "POST",
-	url: "/users/create",
-	data: short_password_req,
-	dataType: "json"
-}).done(function(result){
-	console.log(result)
-	short_password_res = result;
-}).fail(function(err){
-	console.log(err);
-	short_password_res = err;
-});
-
-
-/////////////////////
-//email format //
-///////////////////
-var email_format_req = {
-	username : "user1",
-	email: "user1@mit",
-	password: "123",
-	proficiencies: ["English"],
-	desires: ["Spanish"]
-}
-
-var email_format_res;
-
-$.ajax({
-	type: "POST",
-	url: "/users/create",
-	data: email_format_req,
-	dataType: "json"
-}).done(function(result){
-	console.log(result)
-	email_format_res = result;
-}).fail(function(err){
-	console.log(err);
-	email_format_res = err;
-});
-
-
 /////////////////////
 //Create Good User//
 ///////////////////
-var good_user_req = {
-	username : "user1",
-	email: "user1@mit.edu",
-	password: "password1",
-	proficiencies: ["English"],
-	desires: ["Spanish"]
-}
+QUnit.test("good user", function(assert) {
 
-var good_user_res;
+	var random_num = String(Math.random()).slice(2);
+	
+	var good_user_req = {
+		username : random_num,
+		email: random_num+"@mit.edu",
+		password: "password1",
+		proficiencies: ["English"],
+		desires: ["Spanish"]
+	}
 
-$.ajax({
-	type: "POST",
-	url: "/users/create",
-	data: good_user_req,
-	dataType: "json"
-}).done(function(result){
-	console.log(result)
-	good_user_res = result;
-}).fail(function(err){
-	console.log(err);
-	good_user_res = err;
+	var good_user_res = "not yet defined";
+
+	$.ajax({
+		type: "POST",
+		url: "/users/create",
+		data: good_user_req,
+		dataType: "json"
+	}).done(function(result){
+		good_user_res = result;
+	}).fail(function(err){
+		good_user_res = err;
+	});	
+
+	assert.ok(good_user_req.username == random_num,"created user SUCCESS ");
+
 });
 
 
 
-///////////////////
-//Edit User Test//
-/////////////////
-var edit_req = {
-	proficiencies: ["English","Arabic"],
-	desires: ["Spanish","Portuguese"]
-}
+// /////////////////////
+// //duplicate email//
+// ///////////////////
+QUnit.test("duplicate email", function(assert) {
 
-var edit_res;
+	var random_num = String(Math.random()).slice(2);
+	
+	var duplicate_email_req = {
+		username : random_num,
+		email: "repeat@mit.edu",
+		password: "password1",
+		proficiencies: ["English"],
+		desires: ["Spanish"]
+	}
 
-$.ajax({
-	type: "POST",
-	url: "/users/edit",
-	data: edit_req,
-	dataType: "json"
-}).done(function(result){
-	console.log(result)
-	edit_res = result;
-}).fail(function(err){
-	console.log(err);
-	edit_res = err;
+	var duplicate_email_res = 0;
+
+	$.ajax({
+		type: "POST",
+		url: "/users/create",
+		data: duplicate_email_req,
+		dataType: "json"
+	}).done(function(result){
+		duplicate_email_res = 1;
+	}).fail(function(err){
+		duplicate_email_res = 0;
+	});	
+
+	assert.ok(duplicate_email_res == 0,"create user failed ");
+
 });
 
-///////////////////
-//Get User Page///
-/////////////////
-var user_page_req = {
-	user : good_user_res
-}
+// /////////////////////
+// //duplicate username//
+// ///////////////////
+QUnit.test("duplicate username", function(assert) {
 
-var user_page_res;
+	var random_num = String(Math.random()).slice(2);
+	
+	var duplicate_username_req = {
+		username : "repeat",
+		email: "random_num@mit.edu",
+		password: "password1",
+		proficiencies: ["English"],
+		desires: ["Spanish"]
+	}
 
-$.ajax({
-	type: "GET",
-	url: "/users/",
-	data: user_page_req,
-	dataType: "json"
-}).done(function(result){
-	console.log(result)
-	user_page_res=result;
-}).fail(function(err){
-	console.log(err);
-	user_page_res=err;
+	var duplicate_username_res = 0;
+
+	$.ajax({
+		type: "POST",
+		url: "/users/create",
+		data: duplicate_username_req,
+		dataType: "json"
+	}).done(function(result){
+		duplicate_username_res = 1;
+	}).fail(function(err){
+		duplicate_username_res = 0;
+	});	
+
+	assert.ok(duplicate_username_res == 0,"create user failed ");
+
 });
 
 
-/////////////////////
-//Get Translation///
-///////////////////
-// var req4 = {
-// 	query : {
-// 		text : "hello",
-// 		language1 : "English",
-// 		language2 : "Spanish"
-// 	}
+
+// /////////////////////
+// //short password//
+// ///////////////////
+QUnit.test("short password", function(assert) {
+
+	var random_num = String(Math.random()).slice(2);
+	
+	var short_password_req = {
+		username : random_num,
+		email: random_num+"@mit.edu",
+		password: "short",
+		proficiencies: ["English"],
+		desires: ["Spanish"]
+	}
+
+	var short_password_res = 0;
+
+	$.ajax({
+		type: "POST",
+		url: "/users/create",
+		data: short_password_req,
+		dataType: "json"
+	}).done(function(result){
+		short_password_res = 1;
+	}).fail(function(err){
+		short_password_res = 0;
+	});	
+
+	assert.ok(short_password_res == 0,"create user failed ");
+
+});
+
+
+
+// /////////////////////
+// //email format //
+// ///////////////////
+QUnit.test("email format", function(assert) {
+
+	var random_num = String(Math.random()).slice(2);
+	
+	var email_format_req = {
+		username : random_num,
+		email: random_num+"@mit",
+		password: "password1",
+		proficiencies: ["English"],
+		desires: ["Spanish"]
+	}
+
+	var email_format_res = 0;
+
+	$.ajax({
+		type: "POST",
+		url: "/users/create",
+		data: email_format_req,
+		dataType: "json"
+	}).done(function(result){
+		email_format_res = 1;
+	}).fail(function(err){
+		email_format_res = 0;
+	});	
+
+	assert.ok(email_format_res == 0,"create user failed ");
+
+});
+
+
+// ///////////////////
+// //Edit User Test//
+// /////////////////
+// var edit_req = {
+// 	proficiencies: ["English","Arabic"],
+// 	desires: ["Spanish","Portuguese"]
 // }
 
-// var res4;
+// var edit_res;
 
 // $.ajax({
-// 	type: "GET",
-// 	url: "/translator/translate",
-// 	data: req4,
+// 	type: "POST",
+// 	url: "/users/edit",
+// 	data: edit_req,
 // 	dataType: "json"
 // }).done(function(result){
 // 	console.log(result)
-// 	res4=result;
+// 	edit_res = result;
 // }).fail(function(err){
 // 	console.log(err);
-// 	res4=err;
+// 	edit_res = err;
+// });
+
+// ///////////////////
+// //Get User Page///
+// /////////////////
+// var user_page_req = {
+// 	user : good_user_res
+// }
+
+// var user_page_res;
+
+// $.ajax({
+// 	type: "GET",
+// 	url: "/users/",
+// 	data: user_page_req,
+// 	dataType: "json"
+// }).done(function(result){
+// 	console.log(result)
+// 	user_page_res=result;
+// }).fail(function(err){
+// 	console.log(err);
+// 	user_page_res=err;
 // });
 
 
-////////////////////////
-//Get Translate Page///
-//////////////////////
-var req5 = {}
+// /////////////////////
+// //Get Translation///
+// ///////////////////
+// // var req4 = {
+// // 	query : {
+// // 		text : "hello",
+// // 		language1 : "English",
+// // 		language2 : "Spanish"
+// // 	}
+// // }
 
-var res5;
+// // var res4;
 
-$.ajax({
-	type: "GET",
-	url: "/translator/",
-	data: req5,
-	dataType: "json"
-}).done(function(result){
-	console.log(result)
-	res5=result;
-}).fail(function(err){
-	console.log(err);
-	res5=err;
-});
-
-
-////////////////////////
-//Get Index Page///////
-//////////////////////
-var req6 = {}
-
-var res6;
-
-$.ajax({
-	type: "GET",
-	url: "/",
-	data: req6,
-	dataType: "json"
-}).done(function(result){
-	console.log(result)
-	res6=result;
-}).fail(function(err){
-	console.log(err);
-	res6=err;
-});
+// // $.ajax({
+// // 	type: "GET",
+// // 	url: "/translator/translate",
+// // 	data: req4,
+// // 	dataType: "json"
+// // }).done(function(result){
+// // 	console.log(result)
+// // 	res4=result;
+// // }).fail(function(err){
+// // 	console.log(err);
+// // 	res4=err;
+// // });
 
 
-////////////////////////
-//Get Home Page////////
-//////////////////////
-var req9 = {}
+// ////////////////////////
+// //Get Translate Page///
+// //////////////////////
+// var translate_req = {}
 
-var res9;
+// var translate_res;
 
-$.ajax({
-	type: "GET",
-	url: "/home/",
-	data: req9,
-	dataType: "json"
-}).done(function(result){
-	console.log(result)
-	res9=result;
-}).fail(function(err){
-	console.log(err);
-	res9=err;
-});
-
-
-
-////////////////////////
-//Logout User//////////
-//////////////////////
-var req7 = {
-	user : {
-		_id : 123
-	}
-}
-
-var res7;
-
-$.ajax({
-	type: "GET",
-	url: "/logout",
-	data: req7,
-	dataType: "json"
-}).done(function(result){
-	console.log(result)
-	res7=result;
-}).fail(function(err){
-	console.log(err);
-	res7=err;
-});
-
-////////////////////////
-//Login User///////////
-//////////////////////
-var req8 = {}
-
-var res8;
-
-$.ajax({
-	type: "POST",
-	url: "/login",
-	data: req8,
-	dataType: "json"
-}).done(function(result){
-	console.log(result)
-	res8=result;
-}).fail(function(err){
-	console.log(err);
-	res8=err;
-});
+// $.ajax({
+// 	type: "GET",
+// 	url: "/translator/",
+// 	data: translate_req,
+// 	dataType: "json"
+// }).done(function(result){
+// 	console.log(result)
+// 	translate_res=result;
+// }).fail(function(err){
+// 	console.log(err);
+// 	translate_res=err;
+// });
 
 
-//////////////////////////////////////
-//Unit Tests Below check Assertions//
-////////////////////////////////////
-test( "Unit Tests", function( assert ) {
-  assert.ok( createsuccess == 200, "create user" );
-  assert.ok( createerror == undefined, "edit user" );
-  assert.ok( res3 == 200, "get user page" );
-  //assert.ok( res4 == 200, "get translation" );
-  assert.ok( res5 == 5, "get translate page" );
-  assert.ok( res6 == 6, "get index page" );
-  assert.ok( res9 == 9, "get index page" );
-  assert.ok( res7 == 7, "get logout page" );
-  assert.ok( res8 == 8, "get login page" );
-});
+// ////////////////////////
+// //Get Index Page///////
+// //////////////////////
+// var req6 = {}
+
+// var res6;
+
+// $.ajax({
+// 	type: "GET",
+// 	url: "/",
+// 	data: req6,
+// 	dataType: "json"
+// }).done(function(result){
+// 	console.log(result)
+// 	res6=result;
+// }).fail(function(err){
+// 	console.log(err);
+// 	res6=err;
+// });
+
+
+// ////////////////////////
+// //Get Home Page////////
+// //////////////////////
+// var req9 = {}
+
+// var res9;
+
+// $.ajax({
+// 	type: "GET",
+// 	url: "/home/",
+// 	data: req9,
+// 	dataType: "json"
+// }).done(function(result){
+// 	console.log(result)
+// 	res9=result;
+// }).fail(function(err){
+// 	console.log(err);
+// 	res9=err;
+// });
+
+
+
+// ////////////////////////
+// //Logout User//////////
+// //////////////////////
+// var req7 = {
+// 	user : {
+// 		_id : 123
+// 	}
+// }
+
+// var res7;
+
+// $.ajax({
+// 	type: "GET",
+// 	url: "/logout",
+// 	data: req7,
+// 	dataType: "json"
+// }).done(function(result){
+// 	console.log(result)
+// 	res7=result;
+// }).fail(function(err){
+// 	console.log(err);
+// 	res7=err;
+// });
+
+// ////////////////////////
+// //Login User///////////
+// //////////////////////
+// var req8 = {}
+
+// var res8;
+
+// $.ajax({
+// 	type: "POST",
+// 	url: "/login",
+// 	data: req8,
+// 	dataType: "json"
+// }).done(function(result){
+// 	console.log(result)
+// 	res8=result;
+// }).fail(function(err){
+// 	console.log(err);
+// 	res8=err;
+// });
